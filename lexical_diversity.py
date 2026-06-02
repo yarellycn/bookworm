74e437067e161593138f974d6757f289159468dd
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-06-02 17:55:36 +0200

    [FEAT]Add lexical diversity function

---
+++ lexical_diversity.py
@@ -0,0 +1,53 @@
+from nltk import FreqDist, word_tokenize
+
+import tools
+
+def number_of_word_tokens(tokens):
+    """Return the number of word tokens in the book."""
+    return len(tokens)
+
+def frequency_count(tokens):
+    """Return the frequency count of each word in the book."""
+    frequency_count = FreqDist(word for word in tokens)
+    return frequency_count
+
+def number_of_unique_word_tokens(tokens):
+    """Return the number of unique words in the book."""
+    return len(set(tokens))
+
+def number_of_tokens_occurring_once(tokens):
+    frequency = frequency_count(tokens)
+    tokens_occurring_once = [word for word, count in frequency.items() if count == 1]
+    return len(tokens_occurring_once)
+
+def ratio_of_unique_word_tokens_to_word_tokens(tokens):
+    ratio = number_of_unique_word_tokens(tokens) / number_of_word_tokens(tokens)
+    return ratio
+
+def average_word_length(tokens):
+    return sum(len(word) for word in tokens) / len(tokens)
+
+def ratio_of_word_tokens_to_unique_word_tokens(tokens):
+    ratio = number_of_word_tokens(tokens) / number_of_unique_word_tokens(tokens)
+    return ratio
+
+def get_lexical_diversity():
+    """Return the lexical diversity of a book."""
+
+    # book_content = download_book(book_id)
+    file = tools.read_text_file("test.txt")
+    tokens = word_tokenize(file)
+    cleaned_tokens = tools.cleaner(tokens)
+
+    return {
+        "tok": int(number_of_word_tokens(cleaned_tokens)),
+        "typ": int(number_of_unique_word_tokens(cleaned_tokens)),
+        "hap": int(number_of_tokens_occurring_once(cleaned_tokens)),
+        "ttr": float(ratio_of_unique_word_tokens_to_word_tokens(cleaned_tokens)),
+        "mwl": float(average_word_length(cleaned_tokens)),
+        "mwf": float(ratio_of_word_tokens_to_unique_word_tokens(cleaned_tokens))
+    }
+
+if __name__ == "__main__":
+    result = get_lexical_diversity()
+    print(result)
--- tools.py
+++ tools.py
@@ -1,2 +1,27 @@
-# def lexical_diversity(text):
+import string
+from urllib.request import urlopen
+
+def download_book(book_id):
+    """Downloads the text of the book with the given ID and saves it."""
+    # f stands for format string, which allows us to insert variables into the string.
+    url = f"https://www.gutenberg.org/files/{book_id}/{book_id}-0.txt"
+
+    # Connect to the URL
+    response = urlopen(url)
+
+    # Read the content of the response
+    text = response.read().decode("utf-8")
+
+    filename = f"{book_id}_book.txt"
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+
+    return filename
+
+def read_text_file(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
     
+def cleaner(tokens):
+    return [word.lower() for word in tokens if word not in string.punctuation]
