4627115dca55f9ce9c383e6733a96b917d8f6429
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-06-04 09:54:08 +0200

    [FEAT]Add entities option (complete and functional)

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import argparse, sys, os
 import topic_modeling as topic
 import lexical_diversity as lexdiv
+import entities
 
 main_file = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if main_file not in sys.path:
@@ -16,7 +17,7 @@ def cli():
     groupe = parser.add_mutually_exclusive_group(required=True)
     groupe.add_argument("--lexdiv", type=int)
     groupe.add_argument("--topics", action="store_true")
-    groupe.add_argument("--entities", action="store_true")
+    groupe.add_argument("--entities", type=int)
     groupe.add_argument("--summarize", action="store_true")
     groupe.add_argument("--similar", action="store_true")
     groupe.add_argument("--card", action="store_true")
@@ -35,6 +36,7 @@ def cli():
         return
 
     elif args.entities:
+        print(entities.get_entities(args.entities))
         return
 
     elif args.summarize:
