77c6c2a08b9fd8619a1dc2314c4f676202d010b9
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-06-05 17:17:40 +0200

    [UPDATE]Spacy entity function is now secondary and the nltk function is the main one

--- entities.py
+++ entities.py
@@ -13,7 +13,6 @@ def clean_entity(name):
     name = name.removeprefix("The ")
     return " ".join(name.split())
 
-
 def is_valid_entity(name):
     bad_words = {
         "chapter", "fig", "extras", "un_important",
@@ -31,41 +30,7 @@ def is_valid_entity(name):
 
     return True
 
-def get_entities_nltk(book_id, action="entitiesnltk"):
-    cached = tools.setup_action(book_id, action)
-
-    if cached is not None:
-        return cached
-
-    # Tokenize the book into sentences.
-    sentences = tools.get_sentences(book_id)
-    characters = set()
-    locations = set()
-
-    for sentence in sentences:
-        tokens = word_tokenize(sentence)
-        tagged_words = pos_tag(tokens)
-        chunked_sentences = ne_chunk(tagged_words)
-
-        for chunk in chunked_sentences:
-            if hasattr(chunk, "label"):
-                name = " ".join(word for word, _ in chunk)
-
-                if chunk.label() == "PERSON":
-                    characters.add(name)
-
-                elif chunk.label() in ["GPE", "LOCATION"]:
-                    locations.add(name)
-
-    result = {
-        "characters": (sorted(characters)),
-        "locations": (sorted(locations))
-    }
-
-    cache.save_cache(book_id, action, result)
-    return result
-
-def get_entities(book_id, action="entities"):
+def get_entities_spacy(book_id, action="spacy"):
     cached = tools.setup_action(book_id, action)
 
     if cached is not None:
@@ -91,6 +56,40 @@ def get_entities(book_id, action="entities"):
 
         elif entity.label_ in ["GPE", "LOC", "FAC"]:
             locations.add(name)
+
+    result = {
+        "characters": (sorted(characters)),
+        "locations": (sorted(locations))
+    }
+
+    cache.save_cache(book_id, action, result)
+    return result
+
+def get_entities(book_id, action="entities"):
+    cached = tools.setup_action(book_id, action)
+
+    if cached is not None:
+        return cached
+
+    # Tokenize the book into sentences.
+    sentences = tools.get_sentences(book_id)
+    characters = set()
+    locations = set()
+
+    for sentence in sentences:
+        tokens = word_tokenize(sentence)
+        tagged_words = pos_tag(tokens)
+        chunked_sentences = ne_chunk(tagged_words)
+
+        for chunk in chunked_sentences:
+            if hasattr(chunk, "label"):
+                name = " ".join(word for word, _ in chunk)
+
+                if chunk.label() == "PERSON":
+                    characters.add(name)
+
+                elif chunk.label() in ["GPE", "LOCATION"]:
+                    locations.add(name)
 
     result = {
         "characters": (sorted(characters)),
