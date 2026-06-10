1609816110c321b8f04b0edab76a53c48141ed02
yarellycn <yarelly.cotrina-navarro-berger@epitech.eu>
2026-06-10 14:07:52 +0200

    [REFACTOR]Improve naming and documentation in summarize module

--- summarize.py
+++ summarize.py
@@ -1,26 +1,32 @@
-import tools
-import cache
-from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.text_rank import TextRankSummarizer
+
+import cache
+import tools
 
 
 def summarize_book(book_id, sentence_count=4):
+    """Generate and return a summary of a book using TextRank."""
     cached = tools.setup_action(book_id, action="summarize")
 
     if cached is not None:
         return cached
 
-    path_book = tools.get_path_file(book_id)
-    lang = tools.get_book_language(path_book)
-    text = tools.header_and_footer_remover(path_book)
+    book_path = tools.get_path_file(book_id)
+    language = tools.get_book_language(book_path)
+    text = tools.header_and_footer_remover(book_path)
 
-    parser = PlaintextParser.from_string(text, Tokenizer(lang))
+    parser = PlaintextParser.from_string(text, Tokenizer(language))
     summarizer = TextRankSummarizer()
 
     best_sentences = summarizer(parser.document, sentence_count)
-    sentence_string = [str(s) for s in best_sentences]
-    chrono_sentences = sorted(sentence_string, key=lambda s: text.find(s))
-    summary_string = " ".join(chrono_sentences)
+
+    summary_sentences = [str(sentence) for sentence in best_sentences]
+    ordered_sentences = sorted(summary_sentences, key=lambda sentence: text.find(sentence))
+
+    summary_string = " ".join(ordered_sentences)
+
     cache.save_cache(book_id, task="summarize", data=summary_string)
+
     return summary_string
